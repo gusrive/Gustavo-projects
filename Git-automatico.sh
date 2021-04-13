ff529bf536e8dd51f827e2866395f71f4b8bd98f
gustavo <gusrive12@gmail.com>
2021-04-13 12:24:00 -0300

    feito

--- Git-automatico.sh
+++ Git-automatico.sh
@@ -10,6 +10,11 @@
 #para rodar ./Git-automatico.sh
 ###########################################
 #---------------- VARIAVEIS --------------------------------#
-
+ADD=$(git add *)
+ATUALIZA=$(git commit -m * "Upload de arquivos feitos - $(date)" )
 #------------------------ FUNÇÃO ---------------------------#
 #------------------------ EXECUÇÃO -------------------------#
+clear
+
+echo "Iniciando atualizacao automatica do github"
+echo
